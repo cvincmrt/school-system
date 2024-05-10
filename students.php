859a011b227822b359b94e52526ebe9b99dbfc8a
cvincmrt <cvincmrt@gmail.com>
2024-05-10 21:20:40 +0200

    Vytvoril som SQL dotaz na databazu a v7sledok som previedol na asociativne pole

--- students.php
+++ students.php
@@ -7,12 +7,23 @@
 
     $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
     
-//vracia popis poslednej chyby prihlasenia-ak je tam neaky text je to true inak false
+//vracia popis poslednej chyby prihlasenia - ak je tam neaky text je to true inak false
     if(mysqli_connect_error()){
         echo mysqli_connect_error();
         exit();
     }
 
-    echo "Vsetko je ok";
+    echo "Database connected...";
+
+//vytvoreny dotaz na tabulku student
+    $sql = "SELECT first_name, second_name FROM student";
+
+//odosleme dotaz na databazu    
+    $result = mysqli_query($conn, $sql);
+
+//
+    $students = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    print_r($students);
+
 
 ?>
