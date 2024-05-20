18a706616d576e69ed6cd4ebc5546b813dc5a628
cvincmrt <cvincmrt@gmail.com>
2024-05-20 19:38:40 +0200

    Upravil som header.php v ktorom som pridalmenu

--- assets/header.php
+++ assets/header.php
@@ -1,3 +1,9 @@
 <header>
-        <h1>HEADER</h1> 
+        <nav>
+            <ul>
+                <li><a href="index.php">Domovska stranka</a></li>
+                <li><a href="students.php">Zoznam studentov</a></li>
+                <li><a href="add-student.php">Pridaj studenta</a></li>
+            </ul>
+        </nav> 
 </header>
